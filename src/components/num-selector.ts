b19b80bfd3e657e345623305307292167ad30e31
watanabe <s.watanabe@sdk-co.com>
2020-10-05 16:57:22 +0900

    flatpickr追加 依存関係バージョンアップ 計算を全面的に修正

--- src/components/num-selector.ts
+++ src/components/num-selector.ts
@@ -5,6 +5,7 @@ export class NumSelector {
     @bindable({ mode: BindingMode.toView }) max = 100;
     @bindable({ mode: BindingMode.twoWay }) private _num = 0;
     @bindable({ mode: BindingMode.toView }) suffix = "Select";
+    @bindable({ mode: BindingMode.toView }) step = 1;
 
     get num():number { return this._num; }
     set num(value: number) { this._num = Number(value);}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import Aurelia from 'aurelia';
 import { MyApp } from './my-app';
 import * as globalComponents from './components/registry';
 import * as globalValueConverters from './value-converters/registry';
+import 'flatpickr/dist/flatpickr.min.css';
 
 Aurelia
   .register(
